389f597c530551b1551f2386dcb14d4dbd321e6e
Cody Frazer <frazercr@gmail.com>
2019-07-04 23:24:22 -0400

    Add reprocessing yield function

--- app/models/inventory_type.rb
+++ app/models/inventory_type.rb
@@ -5,4 +5,13 @@ class InventoryType < ApplicationRecord
   has_many :inventory_type_reactions, foreign_key: :typeID
   has_many :materials, through: :inventory_type_materials, source: :inventory_type
   has_many :reactions, through: :inventory_type_reactions, source: :inventory_type
+
+  def reprocessing_yield(efficiency = 0.50)
+    inventory_type_materials.map do |inventory_type_material|
+      {
+        inventory_type: inventory_type_material.inventory_type,
+        quantity: (inventory_type_material.quantity * efficiency).to_i,
+      }
+    end
+  end
 end
--- test/models/inventory_type_test.rb
+++ test/models/inventory_type_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class InventoryTypeTest < ActiveSupport::TestCase
-  test "should use invTypes table" do
+  test 'should use invTypes table' do
     assert_equal 'invTypes', InventoryType.table_name
+  end
+
+  test 'should give reprocessing yield' do
+    inventory_type = inventory_types(:large_rudimentary_concussion_bomb_i)
+    reprocessing_yield = inventory_type.reprocessing_yield
+
+    assert_equal 6, reprocessing_yield.size
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:tritanium), quantity: 538 }
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:pyerite), quantity: 472 }
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:mexallon), quantity: 435 }
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:isogen), quantity: 424 }
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:nocxium), quantity: 505 }
+
+    assert_includes reprocessing_yield,
+      { inventory_type: inventory_types(:zydrine), quantity: 26 }
   end
 end
