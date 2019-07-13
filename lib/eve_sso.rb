77ee1a631c92c36b6793ac564077caf6b725139e
Cody Frazer <frazercr@gmail.com>
2019-07-13 05:14:52 -0400

    Implement Eve SSO access token request

--- lib/eve_sso.rb
+++ lib/eve_sso.rb
@@ -2,13 +2,46 @@ class EveSSO
   def self.authorization_url
     query_params = {
       response_type: :code,
-      redirect_uri: Rails.configuration.x.eve_sso.callback_url,
-      client_id: Rails.application.credentials.eve_sso[:client_id],
-      scope: Rails.configuration.x.eve_sso.scopes.join(' '),
+      redirect_uri: callback_url,
+      client_id: client_id,
+      scope: scopes.join(' ')
     }
 
-    URI::HTTPS.build(host: Rails.configuration.x.eve_sso.host,
-                     path: '/oauth/authorize',
-                     query: query_params.to_query)
+    build_url(path: '/oauth/authorize', query: query_params.to_query)
+  end
+
+  def self.request_access_token(authorization_code)
+    uri = build_url(path: '/oauth/token')
+    request = Net::HTTP::Post.new(uri)
+    request.basic_auth(client_id, secret_key)
+
+    # NOTE: WebMock reorders encoded body fields, so tests will fail if this hash key order changes
+    request.set_form_data(code: authorization_code, grant_type: :authorization_code)
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+  end
+
+  def self.callback_url
+    Rails.configuration.x.eve_sso.callback_url
+  end
+
+  def self.client_id
+    Rails.application.credentials.eve_sso[:client_id]
+  end
+
+  def self.secret_key
+    Rails.application.credentials.eve_sso[:secret_key]
+  end
+
+  def self.scopes
+    Rails.configuration.x.eve_sso.scopes
+  end
+
+  private
+
+  def self.build_url(options)
+    URI::HTTPS.build(options.merge(host: Rails.configuration.x.eve_sso.host))
   end
 end
--- spec/lib/eve_sso_spec.rb
+++ spec/lib/eve_sso_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe EveSSO do
   let(:host) { 'sisilogin.testeveonline.com' }
   let(:scopes) { %w[esi-markets.structure_markets.v1] }
   let(:callback_url) { 'http://localhost:3000/authenticate' }
-  let(:configuration) { OpenStruct.new({ host: host,
-                                         scopes: scopes,
-                                         callback_url: callback_url }) } 
+  let(:configuration) { OpenStruct.new({ host: host, scopes: scopes, callback_url: callback_url }) }
 
-  let(:client_id) { 'SSO_CLIENT_ID' }
-  let(:secret_key) { 'SSO_SECRET_KEY' }
+  let(:client_id) { '3rdparty_clientid' }
+  let(:secret_key) { 'jkfopwkmif90e0womkepowe9irkjo3p9mkfwe' }
   let(:credentials) { { client_id: client_id, secret_key: secret_key } }
 
   before(:example) do
@@ -25,14 +23,46 @@ RSpec.describe EveSSO do
                           client_id: client_id,
                           scope: scopes.join(' ') }
 
-      expected_url = URI::HTTPS.build(host: host,
-                                      path: '/oauth/authorize',
-                                      query: expected_params.to_query)
-
+      expected_url = URI::HTTPS.build(host: host, path: '/oauth/authorize', query: expected_params.to_query)
       expect(EveSSO.authorization_url).to eq(expected_url)
     end
   end
 
-  # describe '.request_access_token' do
-  # end
+  describe '.request_access_token' do
+    let(:authorization_code) { SecureRandom::urlsafe_base64 }
+    let(:access_token) { SecureRandom::urlsafe_base64 }
+    let(:refresh_token) { SecureRandom::urlsafe_base64 }
+    let(:token_url) { URI::HTTPS.build(host: host, path: '/oauth/token') }
+
+    let(:request_headers) { {
+      'Accept' => '*/*',
+      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Content-Type' => 'application/x-www-form-urlencoded',
+      'Host' => 'sisilogin.testeveonline.com',
+      'User-Agent' => 'Ruby'
+    } }
+
+    let(:request_body) { { code: authorization_code,  grant_type: 'authorization_code' } }
+
+    before(:example) do
+      response_body = {
+        access_token: access_token,
+        token_type: :Bearer,
+        expires_in: 1200,
+        refresh_token: refresh_token
+      }
+
+      stub_request(:post, "https://sisilogin.testeveonline.com/oauth/token")
+          .with(basic_auth: [client_id, secret_key], headers: request_headers, body: request_body)
+          .to_return(status: 200, body: response_body.to_json, headers: {})
+    end
+
+    it 'makes the correct POST request to the EVE SSO server' do
+      EveSSO.request_access_token(authorization_code)
+
+      expect(WebMock).to have_requested(:post, token_url)
+          .with(basic_auth: [client_id, secret_key], headers: request_headers, body: request_body.to_query
+      )
+    end
+  end
 end
