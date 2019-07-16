511d397f56c822bc7958b768a049e123eaf7f8df
Cody Frazer <frazercr@gmail.com>
2019-07-16 16:14:43 -0400

    Add basic EFT parser

---
+++ lib/parsers/eft_parser.rb
@@ -0,0 +1,45 @@
+module Parsers
+  class EftParser
+    def self.parse(fitting_text)
+      items = {}
+
+      ship, name = parse_header(fitting_text.lines.first)
+      items[ship] = 1
+
+      fitting_text.lines[1..-1].each do |line|
+        next if ignore_line?(line)
+
+        parse_line(line).each do |inventory_type, quantity|
+          items[inventory_type] = 0 unless items.has_key?(inventory_type)
+          items[inventory_type] += quantity
+        end
+      end
+
+      { name: name, items: items }
+    end
+
+    def self.ignore_line?(line)
+      line.blank? || line[0..5] == '[Empty'
+    end
+
+    def self.parse_line(line)
+      items = {}
+
+      if line_match = line.match(/^(.*?)\s+x(\d+)$/)
+        items[line_match[1]] = Integer(line_match[2])
+      else
+        items[line.strip] = 1
+      end
+
+      items
+    end
+
+    def self.parse_header(line)
+      if header_match = line.match(/\[(.*?),\s+(.*?)\]/)
+        header_match[1..2]
+      else
+        throw 'Invalid EFT header'
+      end
+    end
+  end
+end
---
+++ spec/lib/parsers/eft_parser_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+require 'parsers/eft_parser'
+
+RSpec.describe Parsers::EftParser do
+  describe '.parse' do
+    subject { described_class.parse(fitting_text) }
+
+    context 'when given a full, valid fitting' do
+      let(:fitting_text) { file_fixture('eft_fittings/tristan.txt').read }
+
+      it 'returns a list of items in the fit with quantity' do
+        expect(subject[:name]).to eq('Full Fit')
+        expect(subject[:items]).to include(
+          'Tristan' => 1,
+          'Drone Damage Amplifier II' => 1,
+          'Overdrive Injector System II' => 1,
+          'Micro Auxiliary Power Core I' => 1,
+          '5MN Y-T8 Compact Microwarpdrive' => 1,
+          'Adaptive Invulnerability Field II' => 1,
+          'Medium Shield Extender II' => 1,
+          'Entosis Link I' => 1,
+          '75mm Gatling Rail II' => 2,
+          'Small Core Defense Field Extender I' => 2,
+          'Small Polycarbon Engine Housing I' => 1,
+          'Hornet EC-300' => 3,
+          'Warrior II' => 5,
+          "Eifyr and Co. 'Rogue' Navigation NN-601" => 1,
+          'Standard Frentix Booster' => 1,
+          'Caldari Navy Lead Charge S' => 400,
+          'Strontium Clathrates' => 35
+        )
+      end
+    end
+
+    context 'when given an unfit ship fitting' do
+      let(:fitting_text) { file_fixture('eft_fittings/tristan_unfit.txt').read }
+
+      it 'returns only the ship hull in list of items' do
+        expect(subject[:name]).to eq('Unfit')
+        expect(subject[:items]).to include('Tristan' => 1)
+        expect(subject[:items].size).to eq(1)
+      end
+    end
+  end
+
+  describe '.ignore_line?' do
+    subject { described_class.ignore_line?(line) }
+
+    context 'given an empty slot line' do
+      let(:line) { '[Empty Low Slot]' }
+
+      it 'is ignored' do
+        expect(subject).to be(true)
+      end
+    end
+
+    context 'given an empty line' do
+      let(:line) { '' }
+
+      it 'is ignored' do
+        expect(subject).to be(true)
+      end
+    end
+
+    context 'given a line with only spaces' do
+      let(:line) { " \t\n" }
+
+      it 'is ignored' do
+        expect(subject).to be(true)
+      end
+    end
+  end
+
+  describe '.parse_line' do
+    subject { described_class.parse_line(line) }
+
+    context 'given a valid line without line ending' do
+      let(:line) { 'Tristan' }
+
+      it 'returns a hash with quantity of 1' do
+        expect(subject).to eq({ 'Tristan' => 1 })
+      end
+    end
+
+    context 'given a valid line without line ending' do
+      let(:line) { "Tristan\n" }
+
+      it 'returns a hash with quantity of 1' do
+        expect(subject).to eq({ 'Tristan' => 1 })
+      end
+    end
+  end
+end
